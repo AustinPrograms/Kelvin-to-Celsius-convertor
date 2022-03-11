5c222e6e7506b5dc79fab70a1a2b679dbd1d896a
AustinPrograms <74085550+AustinPrograms@users.noreply.github.com>
2022-03-10 18:21:09 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+kelvin = 273
+
+print("Converting Kelvin(K) to Celsius(C)")
+
+celsius = input("Kelvin you want to convert?: ")
+
+convert = int(celsius)
+
+solve = (convert - kelvin)
+
+print(solve, "kelvin")
